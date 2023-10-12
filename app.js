3740db235e18c9585b2bbe24a495342adb65a610
Haroonmubashir1234 <126398537+Haroonmubashir1234@users.noreply.github.com>
2023-10-12 17:42:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// const a = "input[type=text], input[type=password]"
+
+// function btn() {
+// if ( a == "") { 
+// alert("Please first fill the form")
+// }
+// else(
+// alert("Login Successfully")
+// )
+// }
+
+// function btn() {
+//     document.getElementById('contain').value = alert('LOGIN SUCCESSFULLY')}  
